862f121db03e83f31a432cab8a2b557b89dba632
kale-io <cos+a@iuvo.ai>
2018-11-08 18:23:17 +0000

    Prefer rest params over arguments Use the spread operator to capture a function's arguments instead of using the `arguments` variable

--- src/definitions/modules/checkbox.js
+++ src/definitions/modules/checkbox.js
@@ -638,10 +638,10 @@ $.fn.checkbox = function(parameters) {
             }
           }
         },
-        error: function() {
+        error: function(...params) {
           if(!settings.silent) {
             module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-            module.error.apply(console, arguments);
+            module.error.apply(console, params);
           }
         },
         performance: {
