11b6b38d18831d13b905ca7e5968ccc74e21eb76
kale-io <cos+a@iuvo.ai>
2018-11-08 18:24:47 +0000

    Prefer rest params over arguments Use the spread operator to capture a function's arguments instead of using the `arguments` variable

--- src/definitions/modules/rating.js
+++ src/definitions/modules/rating.js
@@ -313,10 +313,10 @@ $.fn.rating = function(parameters) {
             }
           }
         },
-        error: function() {
+        error: function(...params) {
           if(!settings.silent) {
             module.error = Function.prototype.bind.call(console.error, console, settings.name + ':');
-            module.error.apply(console, arguments);
+            module.error.apply(console, params);
           }
         },
         performance: {
